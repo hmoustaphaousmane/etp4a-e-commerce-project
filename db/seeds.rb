87f1995a1508e9132bb2cf6e3b2b3da3d4d995cd
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-06-29 10:22:41 +0100

    Seed the database using gem faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+Item.delete_all
+
+100.times do 
+  items = Item.create!(title: Faker::Creature::Cat.name, description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4), price: Faker::Number.decimal(l_digits: 2), image_url: Faker::LoremFlickr.image(search_terms: ['cats']))
+end
